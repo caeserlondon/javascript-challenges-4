9691b3cb2e189b6d5ea097cd16a19ebd113a4b2f
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-13 21:54:27 +0700

    challenge 1 solution 2

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 /// CHALLENGE 1 CAPITALIZE THE FIRST LETTER
+/// solution 1
+// function capitaize(str) {
+// 	let newArr = str.toLowerCase().split(" ");
+// 	for (let i = 0; i < newArr.length; i++) {
+// 		newArr[i] =
+// 			newArr[i].substring(0, 1).toUpperCase() + newArr[i].substring(1);
+// 	}
+// 	return newArr.join(" ");
+// }
 
+// console.log(capitaize("caeser al ani"));
+///////////
+// solution 2
 function capitaize(str) {
-	let newArr = str.toLowerCase().split(" ");
-	for (let i = 0; i < newArr.length; i++) {
-		newArr[i] =
-			newArr[i].substring(0, 1).toUpperCase() + newArr[i].substring(1);
-	}
+	let newArr = str
+		.toLowerCase()
+		.split(" ")
+		.map((word) => word[0].toUpperCase().concat(word.substr(1)));
 	return newArr.join(" ");
 }
-
 console.log(capitaize("caeser al ani"));
